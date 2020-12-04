6356f2c28ca5d7d7681d25412f35263f127fae61
Dev Singh <dev@devksingh.com>
2020-12-04 14:34:26 -0600

    Update obs-virtcam.sh

--- files/obs-virtcam.sh
+++ files/obs-virtcam.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 cd $HOME
-mkdir -p obs_build
-cd obs_build
+apt-get -y install obs-studio v4l2loopback-dkms libobs-dev qtbase5-dev cmake
 git clone --recursive https://github.com/obsproject/obs-studio.git
-git clone https://github.com/CatxFish/obs-v4l2sink
+git clone https://github.com/CatxFish/obs-v4l2sink.git
 cd obs-v4l2sink
-mkdir -p build && cd build
+mkdir build && cd build
 cmake -DLIBOBS_INCLUDE_DIR="../../obs-studio/libobs" -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j4
 make install
-mkdir -p /usr/lib/x86_64-linux-gnu/obs-plugins/
-cp /usr/lib/obs-plugins/v4l2sink.so /usr/lib/x86_64-linux-gnu/obs-plugins/
