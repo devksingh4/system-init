d45a8f9e7db8eb87878974c1b5e53c98d298f190
Dev Singh <dev@devksingh.com>
2022-10-25 16:55:38 -0700

    Create config.ps1

---
+++ windows/config.ps1
@@ -0,0 +1,25 @@
+# elevate if needed
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+# Apps use dark theme
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
+
+# Set dark theme
+start-process -filepath "C:\Windows\Resources\Themes\dark.theme"; timeout /t 3; taskkill /im "systemsettings.exe" /f
+
+# Clear Taskbar
+$path =  'C:\Users\dsingh\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\'
+if (Test-Path $file) {
+  Remove-Item -Recurse -Force $path
+}
+
+REG DELETE HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /F
+
+taskkill /f /im explorer.exe
+
+start explorer.exe
