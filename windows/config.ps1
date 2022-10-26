7f0206cfbde9a676ff9cee9cd5acbbff7e5fec4f
Dev Singh <dev@devksingh.com>
2022-10-25 17:15:03 -0700

    Update config.ps1

--- windows/config.ps1
+++ windows/config.ps1
@@ -23,3 +23,7 @@ REG DELETE HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /F
 taskkill /f /im explorer.exe
 
 start explorer.exe
+
+# Bring back old context menu
+REG ADD "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
