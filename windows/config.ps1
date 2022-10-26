71742c0a1c9f2d31affc3c4dcf8df3e6987dbbbb
Dev Singh <dev@devksingh.com>
2022-10-25 21:48:19 -0500

    Update config.ps1

--- windows/config.ps1
+++ windows/config.ps1
@@ -27,3 +27,6 @@ start explorer.exe
 # Bring back old context menu
 REG ADD "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
+# Uninstall OneDrive
+TASKKILL/F /IM OneDrive.exe
+C:\Windows\System32\OneDriveSetup.exe /uninstall /allusers
