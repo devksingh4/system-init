6ccaebd0f3afc999e1342a763eab55496a170f8e
Dev Singh <dev@devksingh.com>
2023-06-18 16:28:38 -0500

    Update bw-backup.sh

--- servers/bw-backup.sh
+++ servers/bw-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SUFFIX=$(date +'-%Y-%m.tar.gz')
-tar --exclude-vcs -czvf /home/ubuntu/bitwarden_gcloud
+tar --exclude-vcs -czvf backup-bw$SUFFIX /home/ubuntu/bitwarden_gcloud/
 rclone copy backup-bw$SUFFIX b2:dsingh-bw-backups
 rm backup-bw$SUFFIX
