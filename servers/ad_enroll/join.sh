312fdcc11675a5ddb8b9709f35f329e83e196d60
Dev Singh <dev@devksingh.com>
2024-01-22 17:42:14 -0600

    Update join.sh

--- servers/ad_enroll/join.sh
+++ servers/ad_enroll/join.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install realmd sssd libnss-sss libpam-sss
 sudo realm join AD.DEVKSINGH.COM --user dsingh
 sudo realm deny --all
 sudo realm permit -g labusers@ad.devksingh.com
